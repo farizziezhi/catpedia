35e3c7d43e6caeee9407286d85e291e629c7cbb0
muh.farizzi <ezhifarizzi@gmail.com>
2025-11-02 13:49:44 +0800

    Fix: Remove unused controller and fix TypeScript errors

--- start/routes.ts
+++ start/routes.ts
@@ -12,7 +12,6 @@ import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 const CatsController = () => import('#controllers/cats_controller')
 const BreedIdentifierController = () => import('#controllers/breed_identifier_controller')
-const BreedIdentifierSimpleController = () => import('#controllers/breed_identifier_simple_controller')
 
 router.get('/', async ({ response }) => {
   const html = await readFile(join(process.cwd(), 'public', 'index.html'), 'utf-8')
